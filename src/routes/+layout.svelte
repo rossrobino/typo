1770a871107fcebb8b69108df4c09330faaa37ea
rossrobino <rbrobino@gmail.com>
2023-05-04 06:52:35 -0700

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import "../app.postcss";
+</script>
+
+<main class="p-4"><slot /></main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { micromark } from "micromark";
+	let md = "";
+</script>
+
+<h1>markdown editor</h1>
+
+<button class="bg-blue-500 p-2 text-white">Open</button>
+
+<div class="grid gap-4 md:grid-cols-2">
+	<div>
+		<textarea class="prose w-full h-screen bg-slate-200 p-2" bind:value={md} />
+	</div>
+	<div>
+		<div class="prose p-2">{@html micromark(md)}</div>
+	</div>
+</div>
