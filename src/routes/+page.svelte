0b1cc20bd5da2e4e7b029e0cc0d8ca86d238d458
rossrobino <rbrobino@gmail.com>
2023-05-04 13:40:00 -0700

    v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,63 @@
 <script lang="ts">
 	import { micromark } from "micromark";
-	let md = "";
+
+	let content = "";
+	let file: File;
+	let fileHandle: FileSystemFileHandle;
+
+	const options: FilePickerOptions = {
+		types: [
+			{
+				description: "markdown",
+				accept: {
+					"markdown/md": [".md", ".mdx"],
+				},
+			},
+		],
+		excludeAcceptAllOption: true,
+	};
+
+	const open = async () => {
+		[fileHandle] = await window.showOpenFilePicker(options);
+		file = await fileHandle.getFile();
+		content = await file.text();
+	};
+
+	const saveAs = async () => {
+		const handle = await window.showSaveFilePicker(options);
+		const writable = await handle.createWritable();
+		await writable.write(content);
+		await writable.close();
+		file = await handle.getFile();
+		fileHandle = handle;
+	};
+
+	const save = async () => {
+		if (fileHandle) {
+			const writable = await fileHandle.createWritable();
+			await writable.write(content);
+			await writable.close();
+		} else {
+			saveAs();
+		}
+	};
 </script>
 
-<h1>markdown editor</h1>
-
-<button class="bg-blue-500 p-2 text-white">Open</button>
-
-<div class="grid gap-4 md:grid-cols-2">
-	<div>
-		<textarea class="prose w-full h-screen bg-slate-200 p-2" bind:value={md} />
+<div class="mb-4 flex items-center justify-between gap-4">
+	<div class="flex gap-4">
+		<button class="bg-blue-500 p-2 text-white" on:click={open}>Open</button>
+		<button class="bg-blue-500 p-2 text-white" on:click={save}>Save</button>
+		<button class="bg-blue-500 p-2 text-white" on:click={saveAs}>Save As</button
+		>
 	</div>
-	<div>
-		<div class="prose p-2">{@html micromark(md)}</div>
-	</div>
+	<h1>{file?.name ? file.name : "md.robino.dev"}</h1>
+</div>
+
+<div class="grid gap-4 sm:grid-cols-2">
+	<textarea
+		class="prose h-full min-h-[100dvh] w-full bg-blue-50 p-2"
+		placeholder="# Title"
+		bind:value={content}
+	/>
+	<div class="prose min-h-[100dvh] p-2">{@html micromark(content)}</div>
 </div>
