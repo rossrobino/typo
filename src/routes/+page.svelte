43d2979600e76c8e20ceb55401fe0af2e2ee1be9
rossrobino <rbrobino@gmail.com>
2023-05-18 13:00:49 -0700

    add view mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import Markdoc from "@markdoc/markdoc";
 
 	let content = "";
+	let viewMode = false;
 	let scrollY: number;
 	let file: File;
 	let fileHandle: FileSystemFileHandle;
@@ -59,11 +60,6 @@
 		}
 	};
 
-	const onInput = async () => {
-		// textArea.style.height = `${textArea.scrollHeight}px`;
-		save();
-	};
-
 	const addContent = async (s: string, inline: boolean) => {
 		const carPos = textArea.selectionEnd;
 		if (inline) {
@@ -84,7 +80,7 @@
 				}
 			}
 		}
-		await onInput(); // have to await to make sure focus/selection works
+		await save(); // have to await to make sure focus/selection works
 		
 		let startPos: number;
 		let endPos: number;
@@ -128,155 +124,178 @@
 
 <svelte:window bind:scrollY />
 
-<header
-	class="flex flex-col-reverse justify-between p-4 sm:flex-row sm:items-center bg-slate-950"
->
-	<nav class="flex flex-wrap">
-		{#if supported}
-			<button class="btn" on:click={open}>Open</button>
-			<button class="btn" on:click={saveAs}>Save&nbsp;As</button>
-		{:else}
-			<a href="data:text/plain,{content}" download="Untitled.md" class="btn">
-				Download
-			</a>
-		{/if}
-		<div class="flex flex-wrap">
-			<div class="group">
-				<button class="btn">H</button>
-				<div
-					class="fixed hidden flex-col rounded-xl bg-slate-700 p-2 transition group-hover:flex"
-				>
-					<button class="btn" on:click={() => addContent("# ", false)}>
-						H1
-					</button>
-					<button class="btn" on:click={() => addContent("## ", false)}>
-						H2
-					</button>
-					<button class="btn" on:click={() => addContent("### ", false)}>
-						H3
-					</button>
-					<button class="btn" on:click={() => addContent("#### ", false)}>
-						H4
-					</button>
+{#if !viewMode}
+	<header
+		class="flex flex-col-reverse justify-between p-4 sm:flex-row sm:items-center bg-slate-950"
+	>
+		<nav class="flex flex-wrap">
+			{#if supported}
+				<button class="btn" on:click={open}>Open</button>
+				<button class="btn" on:click={saveAs}>Save&nbsp;As</button>
+			{:else}
+				<a href="data:text/plain,{content}" download="Untitled.md" class="btn">
+					Download
+				</a>
+			{/if}
+			<div class="flex flex-wrap">
+				<div class="group">
+					<button class="btn">H</button>
+					<div
+						class="fixed hidden flex-col rounded-xl bg-slate-700 p-2 transition group-hover:flex"
+					>
+						<button class="btn" on:click={() => addContent("# ", false)}>
+							H1
+						</button>
+						<button class="btn" on:click={() => addContent("## ", false)}>
+							H2
+						</button>
+						<button class="btn" on:click={() => addContent("### ", false)}>
+							H3
+						</button>
+						<button class="btn" on:click={() => addContent("#### ", false)}>
+							H4
+						</button>
+					</div>
 				</div>
+				<button class="btn" on:click={() => addContent("- ", false)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<title>bullet</title>
+						<path
+							fill-rule="evenodd"
+							d="M6 4.75A.75.75 0 016.75 4h10.5a.75.75 0 010 1.5H6.75A.75.75 0 016 4.75zM6 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H6.75A.75.75 0 016 10zm0 5.25a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H6.75a.75.75 0 01-.75-.75zM1.99 4.75a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1v-.01zM1.99 15.25a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1v-.01zM1.99 10a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1V10z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</button>
+				<button class="btn" on:click={() => addContent("> ", false)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<title>blockquote</title>
+						<path
+							fill-rule="evenodd"
+							d="M10 3c-4.31 0-8 3.033-8 7 0 2.024.978 3.825 2.499 5.085a3.478 3.478 0 01-.522 1.756.75.75 0 00.584 1.143 5.976 5.976 0 003.936-1.108c.487.082.99.124 1.503.124 4.31 0 8-3.033 8-7s-3.69-7-8-7zm0 8a1 1 0 100-2 1 1 0 000 2zm-2-1a1 1 0 11-2 0 1 1 0 012 0zm5 1a1 1 0 100-2 1 1 0 000 2z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</button>
+				<button class="btn" on:click={() => addContent("[text](href)", true)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<title>anchor</title>
+						<path
+							d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z"
+						/>
+						<path
+							d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z"
+						/>
+					</svg>
+				</button>
+				<button class="btn" on:click={() => addContent("![alt](src)", true)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<title>image</title>
+						<path
+							fill-rule="evenodd"
+							d="M1 5.25A2.25 2.25 0 013.25 3h13.5A2.25 2.25 0 0119 5.25v9.5A2.25 2.25 0 0116.75 17H3.25A2.25 2.25 0 011 14.75v-9.5zm1.5 5.81v3.69c0 .414.336.75.75.75h13.5a.75.75 0 00.75-.75v-2.69l-2.22-2.219a.75.75 0 00-1.06 0l-1.91 1.909.47.47a.75.75 0 11-1.06 1.06L6.53 8.091a.75.75 0 00-1.06 0l-2.97 2.97zM12 7a1 1 0 11-2 0 1 1 0 012 0z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</button>
+				<button
+					class="btn"
+					on:click={() =>
+						addContent(
+							"| th | th |\n| -- | -- |\n| td | td |\n| td | td |",
+							true,
+						)}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						class="h-5 w-5"
+					>
+						<title>table</title>
+						<path
+							fill-rule="evenodd"
+							d="M.99 5.24A2.25 2.25 0 013.25 3h13.5A2.25 2.25 0 0119 5.25l.01 9.5A2.25 2.25 0 0116.76 17H3.26A2.267 2.267 0 011 14.74l-.01-9.5zm8.26 9.52v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75v.615c0 .414.336.75.75.75h5.373a.75.75 0 00.627-.74zm1.5 0a.75.75 0 00.627.74h5.373a.75.75 0 00.75-.75v-.615a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75v.625zm6.75-3.63v-.625a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75v.625c0 .414.336.75.75.75h5.25a.75.75 0 00.75-.75zm-8.25 0v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75v.625c0 .414.336.75.75.75H8.5a.75.75 0 00.75-.75zM17.5 7.5v-.625a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75V7.5c0 .414.336.75.75.75h5.25a.75.75 0 00.75-.75zm-8.25 0v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75V7.5c0 .414.336.75.75.75H8.5a.75.75 0 00.75-.75z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				</button>
 			</div>
-			<button class="btn" on:click={() => addContent("- ", false)}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="h-5 w-5"
-				>
-					<title>bullet</title>
-					<path
-						fill-rule="evenodd"
-						d="M6 4.75A.75.75 0 016.75 4h10.5a.75.75 0 010 1.5H6.75A.75.75 0 016 4.75zM6 10a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H6.75A.75.75 0 016 10zm0 5.25a.75.75 0 01.75-.75h10.5a.75.75 0 010 1.5H6.75a.75.75 0 01-.75-.75zM1.99 4.75a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1v-.01zM1.99 15.25a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1v-.01zM1.99 10a1 1 0 011-1H3a1 1 0 011 1v.01a1 1 0 01-1 1h-.01a1 1 0 01-1-1V10z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</button>
-			<button class="btn" on:click={() => addContent("> ", false)}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="h-5 w-5"
-				>
-					<title>blockquote</title>
-					<path
-						fill-rule="evenodd"
-						d="M10 3c-4.31 0-8 3.033-8 7 0 2.024.978 3.825 2.499 5.085a3.478 3.478 0 01-.522 1.756.75.75 0 00.584 1.143 5.976 5.976 0 003.936-1.108c.487.082.99.124 1.503.124 4.31 0 8-3.033 8-7s-3.69-7-8-7zm0 8a1 1 0 100-2 1 1 0 000 2zm-2-1a1 1 0 11-2 0 1 1 0 012 0zm5 1a1 1 0 100-2 1 1 0 000 2z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</button>
-			<button class="btn" on:click={() => addContent("[text](href)", true)}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="h-5 w-5"
-				>
-					<title>anchor</title>
-					<path
-						d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z"
-					/>
-					<path
-						d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z"
-					/>
-				</svg>
-			</button>
-			<button class="btn" on:click={() => addContent("![alt](src)", true)}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="h-5 w-5"
-				>
-					<title>image</title>
-					<path
-						fill-rule="evenodd"
-						d="M1 5.25A2.25 2.25 0 013.25 3h13.5A2.25 2.25 0 0119 5.25v9.5A2.25 2.25 0 0116.75 17H3.25A2.25 2.25 0 011 14.75v-9.5zm1.5 5.81v3.69c0 .414.336.75.75.75h13.5a.75.75 0 00.75-.75v-2.69l-2.22-2.219a.75.75 0 00-1.06 0l-1.91 1.909.47.47a.75.75 0 11-1.06 1.06L6.53 8.091a.75.75 0 00-1.06 0l-2.97 2.97zM12 7a1 1 0 11-2 0 1 1 0 012 0z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</button>
-			<button
-				class="btn"
-				on:click={() =>
-					addContent(
-						"| th | th |\n| -- | -- |\n| td | td |\n| td | td |",
-						true,
-					)}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 20 20"
-					fill="currentColor"
-					class="h-5 w-5"
-				>
-					<title>table</title>
-					<path
-						fill-rule="evenodd"
-						d="M.99 5.24A2.25 2.25 0 013.25 3h13.5A2.25 2.25 0 0119 5.25l.01 9.5A2.25 2.25 0 0116.76 17H3.26A2.267 2.267 0 011 14.74l-.01-9.5zm8.26 9.52v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75v.615c0 .414.336.75.75.75h5.373a.75.75 0 00.627-.74zm1.5 0a.75.75 0 00.627.74h5.373a.75.75 0 00.75-.75v-.615a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75v.625zm6.75-3.63v-.625a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75v.625c0 .414.336.75.75.75h5.25a.75.75 0 00.75-.75zm-8.25 0v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75v.625c0 .414.336.75.75.75H8.5a.75.75 0 00.75-.75zM17.5 7.5v-.625a.75.75 0 00-.75-.75H11.5a.75.75 0 00-.75.75V7.5c0 .414.336.75.75.75h5.25a.75.75 0 00.75-.75zm-8.25 0v-.625a.75.75 0 00-.75-.75H3.25a.75.75 0 00-.75.75V7.5c0 .414.336.75.75.75H8.5a.75.75 0 00.75-.75z"
-						clip-rule="evenodd"
-					/>
-				</svg>
-			</button>
-		</div>
-	</nav>
-	<h1 class="px-4 py-2 font-bold">
-		{file?.name ? file.name : "md"}
-	</h1>
-</header>
+		</nav>
+		<h1 class="px-4 py-2 font-bold">
+			{file?.name ? file.name : "md"}
+		</h1>
+	</header>
+{/if}
 
-<main class="grid max-h-screen grow overflow-hidden sm:grid-cols-2">
-	<textarea
-		class="max-w-none resize-none appearance-none bg-transparent p-4 font-mono transition placeholder:text-slate-400 focus:outline-none"
-		placeholder="# Title"
-		bind:value={content}
-		bind:this={textArea}
-		on:input={onInput}
-	/>
+<main 
+	class="grid max-h-screen grow overflow-hidden {viewMode ? "" : "sm:grid-cols-2"}"
+>
+	{#if !viewMode}
+		<textarea
+			class="max-w-none resize-none appearance-none bg-transparent p-4 font-mono transition placeholder:text-slate-400 focus:outline-none"
+			placeholder="# Title"
+			bind:value={content}
+			bind:this={textArea}
+			on:input={save}
+		/>
+	{/if}
 	<div
-		class="prose prose-slate hidden max-w-none overflow-y-auto bg-slate-100 p-4 text-slate-950 sm:block"
+		class="prose prose-slate max-w-none overflow-y-auto bg-slate-100 p-4 text-slate-950 sm:block"
+		class:hidden={!viewMode}
 	>
 		{@html mdToHtml(content)}
 	</div>
 </main>
 
-<footer class="flex flex-wrap gap-4 p-4 bg-slate-950">
-	<div>
-		{content.length}
-		{content.length === 1 ? "character" : "characters"}
+<footer class="flex justify-between gap-4 p-4 bg-slate-950">
+	<div class="flex flex-wrap gap-4">
+		<div>
+			{content.length}
+			{content.length === 1 ? "character" : "characters"}
+		</div>
+		<div>
+			{wordCount}
+			{wordCount === 1 ? "word" : "words"}
+		</div>
+		<div>
+			{getReadingTime(wordCount)}
+		</div>
 	</div>
-	<div>
-		{wordCount}
-		{wordCount === 1 ? "word" : "words"}
-	</div>
-	<div>
-		{getReadingTime(wordCount)}
-	</div>
+	<button class="btn" on:click={() => viewMode = !viewMode}>
+		{#if viewMode}
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+				<title>edit</title>
+				<path d="M2.695 14.763l-1.262 3.154a.5.5 0 00.65.65l3.155-1.262a4 4 0 001.343-.885L17.5 5.5a2.121 2.121 0 00-3-3L3.58 13.42a4 4 0 00-.885 1.343z" />
+			</svg>
+		{:else}
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+				<title>view</title>
+				<path d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z" />
+				<path fill-rule="evenodd" d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+			</svg>
+		{/if}
+	</button>
 </footer>
 
 <style lang="postcss">
