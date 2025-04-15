abce91499d64e332e02c450e1782d90919238556
rossrobino <rbrobino@gmail.com>
2025-04-15 15:46:27 -0400

    parallel

--- src/lib/util/formatMd.ts
+++ src/lib/util/formatMd.ts
@@ -1,13 +1,14 @@
 export const formatMd = async (md: string) => {
-	const { format } = await import("prettier/standalone");
-
-	const mdPlugin = await import("prettier/plugins/markdown");
-	const { default: estree } = await import("prettier/plugins/estree");
-
-	const js = await import("prettier/plugins/babel");
-	const ts = await import("prettier/plugins/typescript");
-	const css = await import("prettier/plugins/postcss");
-	const html = await import("prettier/plugins/html");
+	const [{ format }, mdPlugin, { default: estree }, js, ts, css, html] =
+		await Promise.all([
+			import("prettier/standalone"),
+			import("prettier/plugins/markdown"),
+			import("prettier/plugins/estree"),
+			import("prettier/plugins/babel"),
+			import("prettier/plugins/typescript"),
+			import("prettier/plugins/postcss"),
+			import("prettier/plugins/html"),
+		]);
 
 	return format(md, {
 		parser: "markdown",
